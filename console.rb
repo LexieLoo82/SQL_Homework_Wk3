7fa4a9b85b6e0095f4400c3f8c7783776bbd6ac9
Alex McIntosh <nz.a.mcintosh@gmail.com>
2018-10-19 13:25:47 +0100

    adds CRUD to films

--- console.rb
+++ console.rb
@@ -1,9 +1,11 @@
 require_relative('models/customer')
 require_relative('models/ticket')
 require_relative('models/film')
+
 require('pry-byebug')
 
-Customer.delete()
+Customer.delete_all()
+Film.delete_all()
 
 
 customer1 = Customer.new('name' => 'Angie', 'funds' => '200')
@@ -15,8 +17,18 @@ customer3.save()
 customer4 = Customer.new('name' => 'Robin', 'funds' => '45')
 customer4.save()
 
-customer1.funds = "120"
-customer1.update()
+
+film1 = Film.new('title' => 'The Fellowship of the Ring', 'price' => '15')
+film1.save()
+film2 = Film.new('title' => 'The Hobbit', 'price' => '15')
+film2.save()
+film3 = Film.new('title' => 'Churchill', 'price' => '15')
+film3.save()
+film4 = Film.new('title' => 'Halloween', 'price' => '15')
+film4.save()
+
+# film2.title = 'The Hobbit: An Unexpected Journey'
+# film2.update
 
 
 binding.pry
--- models/customer.rb
+++ models/customer.rb
@@ -33,7 +33,7 @@ values = [@name, @funds, @id]
 SqlRunner.run(sql, values)
 end
 
-def self.delete()
+def self.delete_all()
   sql = "DELETE FROM customers"
   SqlRunner.run(sql)
 end
